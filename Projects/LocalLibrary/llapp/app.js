60aa2d47a99e5d19d851fc7e89cee27d81a0c14c
Abhinav Mishra <abhinavmish96@gmail.com>
2019-06-27 18:29:35 +0530

    <project>

--- Projects/LocalLibrary/llapp/app.js
+++ Projects/LocalLibrary/llapp/app.js
@@ -11,7 +11,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://admin:<adminpw>@cluster0-wmyzt.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = 'mongodb+srv://admin:adminpw@cluster0-wmyzt.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
